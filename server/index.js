f524673889017062aef14501f23492b0a64a1542
sadath2001 <sadathh44@gmail.com>
2024-10-05 03:48:55 +0530

    initialized sockets

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express=require('express')
 const {createServer} =require('node:http');
+const {join}=require('node:path')
+const {Server}=require('socket.io');
 const app=express()
 const port=4000
-server=createServer(app)
+const server=createServer(app)
+const io=new Server(server)
+
 app.get('/',(req,res)=>{
-res.send('<h1>Hey bro</h1>')
+res.sendFile(join(__dirname,'../index.html'))
 })
 
-Server.listen(port,()=>{
+
+io.on('connection',(socket)=>{
+    console.log('user connected: ',socket.id)
+    socket.on('disconnect',()=>{
+        console.log('user disconnected')
+    })
+})
+server.listen(port,()=>{
     console.log(`listening on: ${port}`)
 })
